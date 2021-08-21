b17b8cbc3c314254990f8257375b7616d7466964
fpestunov <fpestunov@gmail.com>
2021-08-21 13:45:56 +0400

    add lesson7 files

---
+++ public/sandbox.js
@@ -0,0 +1,7 @@
+"use strict";
+var character = "mario";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+inputs.forEach(function (input) {
+  return console.log(input);
+});
--- sandbox.js
+++
@@ -1,4 +0,0 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) { return console.log(input); });
